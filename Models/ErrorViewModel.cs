462f39369689c83d9e1fddb94eeaf5950656c41f
JotaPeCL <juanpcamlo20@gmail.com>
2024-03-03 08:20:30 -0600

    Crear Proyecto nuevo OTRA VEZ

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SWS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
